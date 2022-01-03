2dbc0acd946a8a78290512ff33434e84d121517f
Lcheck <znfhdl789@naver.com>
2022-01-03 18:17:35 +0900

    22-01-03// customer.js 구조화, map을 이용해 컴포넌트 dom(?) 처리

--- src/App.js
+++ src/App.js
@@ -3,26 +3,64 @@ import './App.css';
 import Customer from './components/Customer';
 //만들어둔 커스터머 컴포넌트를 불러옴
 
-const customer = {
+const customers = [{
 
+'id' : 1,
+'image' : 'https://placeimg.com/64/64/any',
 'name':'홍동',
 'birthday':'991223',
 'gender':'남자',
 'job':'군인'
 
-}
+},
+{
+
+  'id' : 2,
+  'image' : 'https://placeimg.com/64/64/any',
+  'name':'은호',
+  'birthday':'891023',
+  'gender':'남자',
+  'job':'학생'
+  
+  },
+  {
+
+    'id' : 3,
+    'image' : 'https://placeimg.com/64/64/any',
+    'name':'채림',
+    'birthday':'790223',
+    'gender':'여자',
+    'job':'BJ'
+    
+    }]
+
+
 class App extends Component {
   render(){
   return (
-    <Customer
-    //props라는 객체로 컴포넌트의 내용을 전달할 수 있다.
-    name = {customer.name}
-    birthday = {customer.birthday}
-    gender = {customer.gender}
-    job = {customer.job}
-    
-    /> //커스터머 컴포넌트를 그리겠다.
+    <div>
+   { customers.map(c => {
+
+      return(
+
+      <Customer
+      key={c.id}
+      //react에서 map을 쓸 때에는 key 프로퍼티를 생성해주어야함
+      id={c.id}
+      image = {c.image}
+      name = {c.name}
+      birthday = {c.birthday}
+      gender = {c.gender}
+      job = {c.job}
+      
+      />  
+
+
+      );
+    }) }
+    </div>
   );
+  //고객의 정보를 객체배열에 담고, map으로 일괄처리
   }
 }
 //리액트는 html문서를 효과적으로 보여주기 위한 도구임.
--- src/components/Customer.js
+++ src/components/Customer.js
@@ -6,21 +6,57 @@ class Customer extends React.Component{
 render(){
 //render메서드는 return값에 내부 컴포넌트를 가져야함
     return (<div>
-
-        <h2>{this.props.name}</h2>
-        <p>{this.props.birthday}</p>
-        <p>{this.props.gender}</p>
-        <p>{this.props.job}</p>
-        {/* props로 프로퍼티 전달 */}
+        <CustomerProfile id={this.props.id} image={this.props.image} name={this.props.name}/>
+        <CustomerInfo birthday={this.props.birthday} gender={this.props.gender} job={this.props.job}/>
     </div>)
 
+    //react의 요소는 jsx문법을 따르는데, 반드시 요소를 div태그로 감싸야 한다는 문법이 있음
+    //
+        //커스터머 컴포넌트를 두개의 컴포넌트로 구성했음
+        //이러한 계층적 구성은, view를 관리하기 용이하게 함
 
 }
+//생각해보니 바벨 문법을 자동으로 쓸 수 있다. 
 //render는 컴포넌트에서 항상 실행되는 함수임.
 //컴포넌트를 그리겠다는 선언을 가짐
 
 }
 
+class CustomerProfile extends React.Component{
+//고객에 대한 정보가 무수히 많을 수 있다.
+//고객의 정보중, 고객의 프로필을 구성하는 정보를 구성하는 클래스이다.
 
+render(){
+
+    return(
+
+        <div>
+        <img src={this.props.image} alt='profile'/>
+        <h2>{this.props.name}({this.props.id})</h2>
+        </div>
+    );
+
+}
+
+
+}
+class CustomerInfo extends React.Component{
+//사용자의 남은 데이터 출력
+render(){
+
+    return(
+
+        <div>
+<p>{this.props.birthday}</p>
+<p>{this.props.gender}</p>
+<p>{this.props.job}</p>
+
+        </div>
+    );
+
+}
+
+
+}
 export default Customer;
 //커스터머 컴포넌트를 내보냄
